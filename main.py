a1f1f4e1e807c8f8ee1e02bc3788dba3fae12b9c
jacobtt21 <jacobt1206@gmail.com>
2022-10-31 15:19:32 -0500

    / endpoint set and ready for postgres connection

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, request, redirect, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!!'
+@app.route('/', methods=['POST', 'GET'])
+def index():
+  if request.method == 'POST':
+      return jsonify({'res' :  "hi there cutie ;)" })
+  else:
+    return redirect("http://www.oustro.xyz", code=302)
+
+if __name__ == '__main__':
+    app.run()
