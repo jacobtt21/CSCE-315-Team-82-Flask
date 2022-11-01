6a133351146817284c08d0812f0981a8bb75ec0e
jacobtt21 <jacobt1206@gmail.com>
2022-11-01 12:25:34 -0500

    updates DB

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from flask import Flask, request, redirect, jsonify
 import psycopg2
 
 app = Flask(__name__)
+connection = psycopg2.connect(user="csce315_908_hollenbeck",password="130009055", host="csce-315-db.engr.tamu.edu", port="5432", database="csce315_908_82")
 
+# https://pynative.com/python-postgresql-tutorial/ <-- tutorial on how to use psycopg2. Refer to this if you have any questions
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
   if request.method == 'POST':
-    connection = psycopg2.connect(user="csce315_908_hollenbeck",password="130009055", host="csce-315-db.engr.tamu.edu", port="5432", database="csce315_908_82")
-    return jsonify({'res' :  connection.get_dsn_parameters()})
+    return jsonify({'res' :  connection.get_dsn_parameters()})   # this returns the stats of the connection and can be changed.
   else:
     return redirect("http://www.oustro.xyz", code=302)
 
