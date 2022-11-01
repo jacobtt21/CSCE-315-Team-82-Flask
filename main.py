88cb1cc31172131e3ecd54888de6fb628c29b7b9
jacobtt21 <jacobt1206@gmail.com>
2022-11-01 12:17:59 -0500

    DB connection set up

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, redirect, jsonify
+import psycopg2
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
   if request.method == 'POST':
-      return jsonify({'res' :  "hi there cutie ;)" })
+    connection = psycopg2.connect(user="csce315_908_hollenbeck",password="130009055", host="csce-315-db.engr.tamu.edu", port="5432", database="csce315_908_82")
+    return jsonify({'res' :  connection.get_dsn_parameters()})
   else:
     return redirect("http://www.oustro.xyz", code=302)
 
